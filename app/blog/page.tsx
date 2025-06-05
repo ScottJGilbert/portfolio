ee5ee39be8e27fc425e0a7836bce12d2520469d4
SirScotterington <scott7gilbert@gmail.com>
2025-06-04 23:27:32 -0500

    Needed to redeploy because of account switch

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1>Blog</h1>
+      <Link href="/new" className="p-4 rounded-2xl bg-slate-800">
+        + New
+      </Link>
     </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1>Projects</h1>
       <hr className="mt-1 mb-3"></hr>
+      <Link href="/new" className="p-4 rounded-2xl bg-slate-800">
+        + New
+      </Link>
     </div>
   );
 }
