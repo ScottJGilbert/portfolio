35c3da032319966a9c019bbb9c866d63e2e11ef7
SirScotterington <scott7gilbert@gmail.com>
2025-09-09 13:39:35 -0500

    Pre-Career Fair Update

--- app/components/general/externals-bar.tsx
+++ app/components/general/externals-bar.tsx
@@ -23,8 +23,6 @@ const links = [
 ];
 
 export default function Contactbar() {
-  const { setIsLoading } = usePageLoading();
-
   return (
     <div>
       <p className="text-lg">External Links</p>
