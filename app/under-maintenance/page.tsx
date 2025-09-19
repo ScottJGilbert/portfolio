a18b400c79491d715c58b5aa800507aa9ff85a2b
SirScotterington <scott7gilbert@gmail.com>
2025-09-19 14:39:23 -0500

    I can't spell

--- app/under-maintenance/page.tsx
+++ app/under-maintenance/page.tsx
@@ -1,13 +1,13 @@
 import { FaFrown } from "react-icons/fa";
 
-export default function MaintencePage() {
+export default function MaintenancePage() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center gap-4 text-center">
       <h1>
         Sorry! <FaFrown className="inline -translate-y-0.5" />
       </h1>
       <h3>
-        The Website is currently under maintence. Please check back later!
+        The Website is currently under maintenance. Please check back later!
       </h3>
     </div>
   );
